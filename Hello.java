b8cc8eb0a6763195e2ab1e463dea133f61b67a3d
alfredocv10 <54364470+alfredocv10@users.noreply.github.com>
2019-08-21 10:10:54 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/************************************
+* Hello
+**************************************
+* Lab 1
+* Alfredo Castro Valdez
+* August 21 2019
+* CMSC 255-003
+************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Alfredo Castro Valdez");
+		System.out.println("Hello");
+		System.out.println("Lab 1");
+		System.out.println("08/21/2019");
+		System.out.println("CMSC 255 003");
+		System.out.println();
+	} 
+
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
